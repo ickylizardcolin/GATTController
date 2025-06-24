11cdb773505e78f460f59e2cb877c6073a395982
Kevin Afryl <ickylizardcolin@gmail.com>
2025-06-24 12:16:00 -0500

    added github

---
+++ app/src/main/java/club/colinthebeardie/gattcontroller/ui/devices/DevicesViewModel.kt
@@ -0,0 +1,13 @@
+package club.colinthebeardie.gattcontroller.ui.devices
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DevicesViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/club/colinthebeardie/gattcontroller/ui/home/HomeViewModel.kt
@@ -0,0 +1,9 @@
+package club.colinthebeardie.gattcontroller.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+}
